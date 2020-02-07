046d08c135f44eddb137458986070439e93f9547
Mike Stratigiou <mike.strat@gmail.com>
2020-02-07 01:02:24 +0100

    blocking requests and clicking inbox button

--- background.js
+++ background.js
@@ -1,28 +1,36 @@
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener(function() {});
 
-});
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    return { cancel: true };
+  },
+  { urls: ["*://browser.pipe.aria.microsoft.com/Collector/3.0/*"] },
+  ["blocking"]
+);
 
 function getMailURL() {
-    return "https://outlook.office.com/mail/inbox";
+  return "https://outlook.office.com/mail";
 }
 
 function isMailURL(url) {
-    return url.startsWith(getMailURL());
+  return url.startsWith(getMailURL());
 }
 
 function repeater() {
-    console.log("Happy croissant..");
-    console.log(Date());
-    chrome.tabs.query({}, function (tabs) {
-        for (let i = 0, tab; tab = tabs[i]; i++) {
-            if (isMailURL(tab.url) && !tab.active) {
-                chrome.tabs.update(tab.id, { url: "https://outlook.office.com/mail/inbox" });
-                console.log("reloaded! id: " + tab.id);
-            }
-
-        }
-    });
-    setTimeout(repeater, 240000);
+  console.log("Happy croissant..");
+  console.log(Date());
+  chrome.tabs.query({}, function(tabs) {
+    for (let i = 0, tab; (tab = tabs[i]); i++) {
+      if (isMailURL(tab.url) && !tab.active) {
+        chrome.tabs.executeScript(tab.id, { file: "utilities.js" });
+        // chrome.tabs.update(tab.id, {
+        //   url: "https://outlook.office.com/mail/inbox"
+        // });
+        console.log("clicked Inbox! id: " + tab.id);
+      }
+    }
+  });
+  setTimeout(repeater, 240000);
 }
 
 setTimeout(repeater, 240000);
---
+++ utilities.js
@@ -0,0 +1,7 @@
+function clickInbox() {
+  console.log("clicked");
+  var inboxBtn = document.querySelector('div[title="Inbox"]');
+  inboxBtn.click();
+}
+
+clickInbox();
